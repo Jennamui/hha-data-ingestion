f4f387442944bb4b8d049189177675f5f6afae2e
Jenna Mui <102179684+Jennamui@users.noreply.github.com>
2022-08-30 00:54:30 -0400

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -17,8 +17,8 @@ import PyPDF2 ## import PyPDF2 for pdf files
 #Section 1
 #Downloaded dataset from Kaggle
 #import local excel file and define each tab 
-tabl = pd.read_excel('/Users/jennamui/Library/Containers/com.microsoft.Excel/Data/Downloads/COVID_diet_dataset.xls', sheet_name='Food_Supply_kcal_Data')
-tab2 = pd.read_excel('/Users/jennamui/Library/Containers/com.microsoft.Excel/Data/Downloads/COVID_diet_dataset.xls', sheet_name='Fat_Supply_Quantity_Data'
+tab1 = pd.read_excel('/Users/jennamui/Library/Containers/com.microsoft.Excel/Data/Downloads/COVID_diet_dataset.xls', sheet_name='Food_Supply_kcal_Data')
+tab2 = pd.read_excel('/Users/jennamui/Library/Containers/com.microsoft.Excel/Data/Downloads/COVID_diet_dataset.xls', sheet_name='Fat_Supply_Quantity_Data')
 
 #Section 2
 #Found an open source json API via CMS
@@ -27,4 +27,21 @@ apiDataset = requests.get('https://data.cms.gov/data-api/v1/dataset/c8a139ee-9e3
 apiDataset = apiDataset.json()
                      
 #Section 3
+#create and load api key, enable api in GCP
+#connect to bigquery
+client = bigquery.Client.from_service_account_json('/Users/jennamui/Documents/GitHub/HHA-507-2022/ingestion/example_files/jenna-507-8d6d73550511.json')
+#get bigquery public dataset
+query_job = client.query("SELECT * FROM `bigquery-public-data.blackhole_database.sdss_dr7` LIMIT 100")
+#get results of query1                   
+results = query_job.result()
+#put results of query1 into dataframe named bigquery1                                        
+bigquery1 = pd.DataFrame(results.to_dataframe())
 
+#repeat for second dataset
+client = bigquery.Client.from_service_account_json('/Users/jennamui/Documents/GitHub/HHA-507-2022/ingestion/example_files/jenna-507-8d6d73550511.json')
+query_job = client.query("SELECT * FROM `bigquery-public-data.chicago_crime.crime` LIMIT 100")
+results = query_job.result()
+bigquery2 = pd.DataFrame(results.to_dataframe())
+
+#had to install db-dtypes
+pip install db-dtypes
